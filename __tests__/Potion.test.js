63e1effdaa1b0b7630b86b76fe5e3079ab1ea2de
zeeshan <mycomputer@9HYNSQ2.hq.target.com>
2022-01-05 06:47:43 -0600

    Create test folder and create test file

---
+++ __tests__/Potion.test.js
@@ -0,0 +1,17 @@
+const potion = require('../lib/Potion.js');
+
+test('creates a random potion object', () => {
+    const potion = new Potion();
+  
+    expect(potion.name).toEqual(expect.any(String));
+    expect(potion.name.length).toBeGreaterThan(0);
+    expect(potion.value).toEqual(expect.any(Number));
+  });
+
+
+
+
+
+
+
+
